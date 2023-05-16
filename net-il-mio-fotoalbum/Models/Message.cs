d0e33888fab6f784bb478a1b00f0b2f3b44a2469
KellerMarika <marikakeller92@gmail.com>
2023-05-16 13:41:07 +0200

    f5 pre migration 1 no message rel

--- net-il-mio-fotoalbum/Models/Message.cs
+++ net-il-mio-fotoalbum/Models/Message.cs
@@ -10,36 +10,40 @@ namespace net_il_mio_fotoalbum.Models
         [Column("id")]
         public int Id { get; set; }
 
+        [Column("created_at")]
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+
         [Column("text")]
         private string Text { get; set; }
 
+      //  private string email;
 
-        private string email;
+        //[Column("email")]
+        //private string Email
+        //{
+        //    get => email;
 
-        [Column("email")]
-        private string Email
-        {
-            get => email;
+        //    set => email = Sender.Email;
+        //}
 
-            set => email = Sender.Email;
-        }   
 
-        [Column("receiver_id")]
-        public string ReceiverId { get; set; }
-        public User Receiver { get; set; }
+        //[Column("Receiver_Id")]
+        //[ForeignKey("Receiver")]
+        //public string ReceiverId { get; set; }
+        //public User Receiver { get; set; }
 
-        [Column("sender_id")]
-        public string SenderId { get; set; }
-        public User Sender { get; set; }
+        //[Column("Sender_Id")]
+        //[ForeignKey("Sender")]
+        //public string SenderId { get; set; }
+        //public User Sender { get; set; }
 
         public Message() { }
         public Message(string text) {  Text = text; }
-        public Message(int id, string text, string receiverId, string senderId)
-        {
-            Id = id;
-            Text = text;
-            ReceiverId = receiverId;
-            SenderId = senderId;
-        }
+        //public Message(string text, string receiverId, string senderId)
+        //{
+        //    Text = text;
+        //    ReceiverId = receiverId;
+        //    SenderId = senderId;
+        //}
     }
 }
--- net-il-mio-fotoalbum/Models/User.cs
+++ net-il-mio-fotoalbum/Models/User.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace net_il_mio_fotoalbum.Models
 {
     public class User : IdentityUser
     {
       public List<Photo>? Photos { get; set; }
-      public List <Message>? Messages { get; set; }
+
+        //[InverseProperty("Sender")]
+        //public List<Message>? SentMessages { get; set; }
+
+        //[InverseProperty("Receiver")]
+        //public List<Message>? ReceivedMessages { get; set; }
+
+      //  public List<Message>? Messages()=> SentMessages?.Concat(ReceivedMessages).OrderBy(m => m.CreationDate).ToList();
     }
 }
